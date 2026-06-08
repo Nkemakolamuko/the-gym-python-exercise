a1cd3032c5b5ebbb72f24f7c36687b3c63d743b7
Nkemakolamuko <nwekevino99@gmail.com>
2026-06-08 19:06:56 +0200

    Update: add solution to question 2 -

--- server_health.py
+++ server_health.py
@@ -7,10 +7,25 @@ def load_list_of_servers():
         config = json.load(f)
     print(f"Loaded {len(config["servers"])} servers")
 
+
+# Solution to question 2
+def send_request_to_one_server():
+    url = "https://httpbin.org/status/200"
+    result = subprocess.run(['curl', '-s', '-o', '/dev/null', '-w', "%{http_code}", url], capture_output=True, text=True)
+
+    output = {
+        "url": url,
+        "status_code" : result.stdout.strip()
+    }
+
+    print(f"{output}")
+
 # result = subprocess.run(['curl', '-l', 'https://httpbin.org/status/200'])
 # print(result.returncode)
 
 if __name__ == "__main__":
     print()
     load_list_of_servers()
+    print()
+    send_request_to_one_server()
 
