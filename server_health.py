1618a37a9e5bd2f9a35b1f90e961ec5038c3ca8b
Nkemakolamuko <nwekevino99@gmail.com>
2026-06-08 18:45:16 +0200

    Create: create solution to question 1 and create the servers.json file -

---
+++ server_health.py
@@ -0,0 +1,15 @@
+import subprocess
+import json
+
+def load_list_of_servers():
+    with open("servers.json", "r") as f:
+        config = json.load(f)
+    print(f"Loaded {len(config["servers"])} servers")
+
+# result = subprocess.run(['curl', '-l', 'https://httpbin.org/status/200'])
+# print(result.returncode)
+
+if __name__ == "__main__":
+    print()
+    load_list_of_servers()
+
